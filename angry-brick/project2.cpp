78f59b676d06134fb7cf42e5831d695553ef70c2
ManuelSerna <manuelserna1997@gmail.com>
2020-01-30 18:26:24 -0600

    drew cube, moving on to mouse callback

--- angry-brick/project2.cpp
+++ angry-brick/project2.cpp
@@ -6,6 +6,18 @@
 #include <iostream>
 #include <cmath>
 
+// Global constants
+#define MINX -99
+#define MAXX 99
+
+#define MINY -99
+#define MAXY 99
+
+#define MINZ -99
+#define MAXZ 99
+
+GLenum mode = GL_POLYGON;
+
 using namespace std;
 
 void init()
@@ -13,25 +25,102 @@ void init()
 	glClearColor(0.0, 0.0, 0.0, 1.0);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	glOrtho(-100, 100, -100, 100, -100, 100);
+	glOrtho(MINX, MAXX, MINY, MAXY, MINZ, MAXZ);
+	
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glRotatef(45.0, 1.0, 1.0, 1.0);// 1st param: angle of rotation (deg)
+	glEnable(GL_DEPTH_TEST);
 }
+
+
+
+void cube(float midx, float midy, float midz, float size)
+{
+   // Define 8 vertices
+   float ax = midx - size / 2;
+   float ay = midy - size / 2;
+   float az = midz + size / 2;
+   float bx = midx + size / 2;
+   float by = midy - size / 2;
+   float bz = midz + size / 2;
+   float cx = midx + size / 2;
+   float cy = midy + size / 2;
+   float cz = midz + size / 2;
+   float dx = midx - size / 2;
+   float dy = midy + size / 2;
+   float dz = midz + size / 2;
+   float ex = midx - size / 2;
+   float ey = midy - size / 2;
+   float ez = midz - size / 2;
+   float fx = midx + size / 2;
+   float fy = midy - size / 2;
+   float fz = midz - size / 2;
+   float gx = midx + size / 2;
+   float gy = midy + size / 2;
+   float gz = midz - size / 2;
+   float hx = midx - size / 2;
+   float hy = midy + size / 2;
+   float hz = midz - size / 2;
+
+   // Draw 6 faces
+   glBegin(mode);
+   glColor3f(1.0, 0.0, 0.0);
+   glVertex3f(ax, ay, az);
+   glVertex3f(bx, by, bz);
+   glVertex3f(cx, cy, cz);
+   glVertex3f(dx, dy, dz);
+   glEnd();
+
+   glBegin(mode);
+   glColor3f(0.0, 1.0, 0.0);
+   glVertex3f(ax, ay, az);
+   glVertex3f(dx, dy, dz);
+   glVertex3f(hx, hy, hz);
+   glVertex3f(ex, ey, ez);
+   glEnd();
+
+   glBegin(mode);
+   glColor3f(0.0, 0.0, 1.0);
+   glVertex3f(ax, ay, az);
+   glVertex3f(ex, ey, ez);
+   glVertex3f(fx, fy, fz);
+   glVertex3f(bx, by, bz);
+   glEnd();
+
+   glBegin(mode);
+   glColor3f(0.0, 1.0, 1.0);
+   glVertex3f(gx, gy, gz);
+   glVertex3f(fx, fy, fz);
+   glVertex3f(ex, ey, ez);
+   glVertex3f(hx, hy, hz);
+   glEnd();
+
+   glBegin(mode);
+   glColor3f(1.0, 0.0, 1.0);
+   glVertex3f(gx, gy, gz);
+   glVertex3f(cx, cy, cz);
+   glVertex3f(bx, by, bz);
+   glVertex3f(fx, fy, fz);
+   glEnd();
+
+   glBegin(mode);
+   glColor3f(1.0, 1.0, 0.0);
+   glVertex3f(gx, gy, gz);
+   glVertex3f(hx, hy, hz);
+   glVertex3f(dx, dy, dz);
+   glVertex3f(cx, cy, cz);
+   glEnd();
+}
+
+
 
 void display()
 {
-	glClear(GL_COLOR_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glMatrixMode(GL_MODELVIEW);
 	
-	// Display square
-	glColor3f(255, 0, 255);
-	
-	glBegin(GL_POLYGON);
-		
-	glVertex2f(20, 20);
-	glVertex2f(-20, 20);
-	glVertex2f(-20, -20);
-	glVertex2f(20, -20);
-	
-	glEnd();
+	cube(0.0, 0.0, 0.0, 10.0);
 	
 	glFlush();
 }
@@ -43,9 +132,11 @@ int main(int argc, char *argv[])
 	glutInit(&argc, argv);
 	glutInitWindowSize(500, 500);
 	glutInitWindowPosition(80, 80);
-	glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
+	glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE | GLUT_DEPTH);
 	glutCreateWindow("Graphics Project 2--Serna-Aguilera");
 	init();
+	//glutIdleFunc(idle):// TODO: maybe include idle callback
+	glutMouseFunc(mouse);
 	glutDisplayFunc(display);
 	glutMainLoop();// enter loop until user closes out of window
 }
