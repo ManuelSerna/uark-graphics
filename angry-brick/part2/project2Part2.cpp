0d14dcf638e6ce057d51fd14ee110246980d24b9
ManuelSerna <manuelserna1997@gmail.com>
2020-02-24 11:37:11 -0600

    included video of demo

--- angry-brick/part2/project2Part2.cpp
+++ angry-brick/part2/project2Part2.cpp
@@ -47,9 +47,9 @@ using namespace std;
 void init()
 {
 	glClearColor(0.0, 0.0, 0.0, 1.0);
-	
+
 	glEnable(GL_DEPTH_TEST);
-	
+
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	glOrtho(MIN_X_VIEW, MAX_X_VIEW, MIN_Y_VIEW, MAX_Y_VIEW, MIN_Z_VIEW, MAX_Z_VIEW);
@@ -61,87 +61,87 @@ void init()
 //================================
 void cube(float midx, float midy, float midz, float size)
 {
-   // Define 8 vertices
-   float ax = midx - size / 2;
-   float ay = midy - size / 2;
-   float az = midz + size / 2;
+	// Define 8 vertices
+	float ax = midx - size / 2;
+	float ay = midy - size / 2;
+	float az = midz + size / 2;
 
-   float bx = midx + size / 2;
-   float by = midy - size / 2;
-   float bz = midz + size / 2;
+	float bx = midx + size / 2;
+	float by = midy - size / 2;
+	float bz = midz + size / 2;
 
-   float cx = midx + size / 2;
-   float cy = midy + size / 2;
-   float cz = midz + size / 2;
+	float cx = midx + size / 2;
+	float cy = midy + size / 2;
+	float cz = midz + size / 2;
 
-   float dx = midx - size / 2;
-   float dy = midy + size / 2;
-   float dz = midz + size / 2;
+	float dx = midx - size / 2;
+	float dy = midy + size / 2;
+	float dz = midz + size / 2;
 
-   float ex = midx - size / 2;
-   float ey = midy - size / 2;
-   float ez = midz - size / 2;
+	float ex = midx - size / 2;
+	float ey = midy - size / 2;
+	float ez = midz - size / 2;
 
-   float fx = midx + size / 2;
-   float fy = midy - size / 2;
-   float fz = midz - size / 2;
+	float fx = midx + size / 2;
+	float fy = midy - size / 2;
+	float fz = midz - size / 2;
 
-   float gx = midx + size / 2;
-   float gy = midy + size / 2;
-   float gz = midz - size / 2;
+	float gx = midx + size / 2;
+	float gy = midy + size / 2;
+	float gz = midz - size / 2;
 
-   float hx = midx - size / 2;
-   float hy = midy + size / 2;
-   float hz = midz - size / 2;
+	float hx = midx - size / 2;
+	float hy = midy + size / 2;
+	float hz = midz - size / 2;
 
-   // Draw 6 faces
-   glBegin(mode);
-   glColor3f(1.0, 0.0, 0.0);
-   glVertex3f(ax, ay, az);
-   glVertex3f(bx, by, bz);
-   glVertex3f(cx, cy, cz);
-   glVertex3f(dx, dy, dz);
-   glEnd();
+	// Draw 6 faces
+	glBegin(mode);
+	glColor3f(1.0, 0.0, 0.0);
+	glVertex3f(ax, ay, az);
+	glVertex3f(bx, by, bz);
+	glVertex3f(cx, cy, cz);
+	glVertex3f(dx, dy, dz);
+	glEnd();
 
-   glBegin(mode);
-   glColor3f(0.0, 1.0, 0.0);
-   glVertex3f(ax, ay, az);
-   glVertex3f(dx, dy, dz);
-   glVertex3f(hx, hy, hz);
-   glVertex3f(ex, ey, ez);
-   glEnd();
+	glBegin(mode);
+	glColor3f(0.0, 1.0, 0.0);
+	glVertex3f(ax, ay, az);
+	glVertex3f(dx, dy, dz);
+	glVertex3f(hx, hy, hz);
+	glVertex3f(ex, ey, ez);
+	glEnd();
 
-   glBegin(mode);
-   glColor3f(0.0, 0.0, 1.0);
-   glVertex3f(ax, ay, az);
-   glVertex3f(ex, ey, ez);
-   glVertex3f(fx, fy, fz);
-   glVertex3f(bx, by, bz);
-   glEnd();
+	glBegin(mode);
+	glColor3f(0.0, 0.0, 1.0);
+	glVertex3f(ax, ay, az);
+	glVertex3f(ex, ey, ez);
+	glVertex3f(fx, fy, fz);
+	glVertex3f(bx, by, bz);
+	glEnd();
 
-   glBegin(mode);
-   glColor3f(0.0, 1.0, 1.0);
-   glVertex3f(gx, gy, gz);
-   glVertex3f(fx, fy, fz);
-   glVertex3f(ex, ey, ez);
-   glVertex3f(hx, hy, hz);
-   glEnd();
+	glBegin(mode);
+	glColor3f(0.0, 1.0, 1.0);
+	glVertex3f(gx, gy, gz);
+	glVertex3f(fx, fy, fz);
+	glVertex3f(ex, ey, ez);
+	glVertex3f(hx, hy, hz);
+	glEnd();
 
-   glBegin(mode);
-   glColor3f(1.0, 0.0, 1.0);
-   glVertex3f(gx, gy, gz);
-   glVertex3f(cx, cy, cz);
-   glVertex3f(bx, by, bz);
-   glVertex3f(fx, fy, fz);
-   glEnd();
+	glBegin(mode);
+	glColor3f(1.0, 0.0, 1.0);
+	glVertex3f(gx, gy, gz);
+	glVertex3f(cx, cy, cz);
+	glVertex3f(bx, by, bz);
+	glVertex3f(fx, fy, fz);
+	glEnd();
 
-   glBegin(mode);
-   glColor3f(1.0, 1.0, 0.0);
-   glVertex3f(gx, gy, gz);
-   glVertex3f(hx, hy, hz);
-   glVertex3f(dx, dy, dz);
-   glVertex3f(cx, cy, cz);
-   glEnd();
+	glBegin(mode);
+	glColor3f(1.0, 1.0, 0.0);
+	glVertex3f(gx, gy, gz);
+	glVertex3f(hx, hy, hz);
+	glVertex3f(dx, dy, dz);
+	glVertex3f(cx, cy, cz);
+	glEnd();
 }
 
 
@@ -152,17 +152,17 @@ void timer(int value)
 {
 	float bounce = -1.0;
 	float step = 0.2;// controls how fast rotation increases by
-	
+
 	// Update velocity
 	cubeX += vX;
 	cubeY += vY;
-	
+
 	// Update rotation
 	if (cubeAngle > 359.9)
 	{
 		cubeAngle = 0.0;
 	}
-	
+
 	// Bounce perfectly off edges of drawing window
 	if (cubeX < MIN_X_VIEW)
 	{
@@ -184,7 +184,7 @@ void timer(int value)
 		vY *= bounce;// hit top of window
 		dAngle += step;
 	}
-	
+
 	// Redraw everything
 	glutPostRedisplay();
 	glutTimerFunc(SLEEP_TIME, timer, 0);
@@ -198,13 +198,13 @@ void idle()
 {
 	// NOTE: idle callback too fast, use timer callback
 	// with faster speed.
-	
+
 	float bounce = -1.0;
-	
+
 	// Update velocity
 	cubeX += vX;
 	cubeY += vY;
-	
+
 	// Bounce perfectly off edges of drawing window
 	if (cubeX < MIN_X_VIEW)
 	{
@@ -240,45 +240,45 @@ float relY = 0.0;
 
 void mouse(int button, int state, int x, int y)
 {
-    // Calculate scale factors
-    float xScale = (MAX_X_VIEW - MIN_X_VIEW)/(float)X_SCREEN;
-    float yScale = (MAX_Y_VIEW - MIN_Y_VIEW)/(float)Y_SCREEN;
-    
-    // Calculate transformed x and y coords: tx and ty
-    float tx = float(MIN_X_VIEW + x * xScale);
-    float ty = float(MIN_Y_VIEW + y * yScale);
+	// Calculate scale factors
+	float xScale = (MAX_X_VIEW - MIN_X_VIEW)/(float)X_SCREEN;
+	float yScale = (MAX_Y_VIEW - MIN_Y_VIEW)/(float)Y_SCREEN;
 
-    // When mouse button is clicked
-    if (state == GLUT_DOWN)
-    {
-    	pressX = tx;
-    	pressY = ty;
-    
-    	// Cube does not move when user is dragging it
+	// Calculate transformed x and y coords: tx and ty
+	float tx = float(MIN_X_VIEW + x * xScale);
+	float ty = float(MIN_Y_VIEW + y * yScale);
+
+	// When mouse button is clicked
+	if (state == GLUT_DOWN)
+	{
+		pressX = tx;
+		pressY = ty;
+
+		// Cube does not move when user is dragging it
 		vX = 0.0;
 		vY = 0.0;
-    }
-    
-    // Mouse button let go
-    if (state == GLUT_UP)
-    {
-    	relX = tx;
-    	relY = ty;
-		
-    	// Calculate change in distance
-    	float dX = relX - pressX;
-    	float dY = relY - pressY;
-    	
-    	// Launch brick in the opposite direction--slingshot
-    	vX = -1.0 * dX / 100.0;
-    	vY = -1.0 * dY / 100.0;
-    	
-    	cubeX = tx;
-    	cubeY = ty;
-    }
+	}
 
-    // Redraw everything
-    glutPostRedisplay();
+	// Mouse button let go
+	if (state == GLUT_UP)
+	{
+		relX = tx;
+		relY = ty;
+		
+		// Calculate change in distance
+		float dX = relX - pressX;
+		float dY = relY - pressY;
+		
+		// Launch brick in the opposite direction--slingshot
+		vX = -1.0 * dX / 100.0;
+		vY = -1.0 * dY / 100.0;
+		
+		cubeX = tx;
+		cubeY = ty;
+	}
+
+	// Redraw everything
+	glutPostRedisplay();
 }
 
 
@@ -287,16 +287,16 @@ void mouse(int button, int state, int x, int y)
 //================================
 void motion(int x, int y)
 {
-    // Calculate scale factors
-    float xScale = (MAX_X_VIEW - MIN_X_VIEW)/(float)X_SCREEN;
-    float yScale = (MAX_Y_VIEW - MIN_Y_VIEW)/(float)Y_SCREEN;
+	// Calculate scale factors
+	float xScale = (MAX_X_VIEW - MIN_X_VIEW)/(float)X_SCREEN;
+	float yScale = (MAX_Y_VIEW - MIN_Y_VIEW)/(float)Y_SCREEN;
 
-    // Handle mouse motion
-    cubeX = MIN_X_VIEW + x * xScale;
-    cubeY = MIN_Y_VIEW + y * yScale;
-    
-    // Redraw everything
-    glutPostRedisplay();
+	// Handle mouse motion
+	cubeX = MIN_X_VIEW + x * xScale;
+	cubeY = MIN_Y_VIEW + y * yScale;
+
+	// Redraw everything
+	glutPostRedisplay();
 }
 
 
@@ -307,25 +307,25 @@ void display()
 {
 	// Clear screen
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	
+
 	// Draw cube
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-    
-    /*
-    Properly draw--translate and rotate--the cube:
-    	- Move center of mass back to origin
-    	- Rotate by desired angle
-    	- Finally, move center of mass back
-    Recall: the last transformation applied in code is the one that's applied first
-    */
-    glTranslatef(cubeX, -cubeY, 0.0);// go back to original pos
-    cubeAngle += dAngle;
+
+	/*
+	Properly draw--translate and rotate--the cube:
+		- Move center of mass back to origin
+		- Rotate by desired angle
+		- Finally, move center of mass back
+	Recall: the last transformation applied in code is the one that's applied first
+	*/
+	glTranslatef(cubeX, -cubeY, 0.0);// go back to original pos
+	cubeAngle += dAngle;
 	glRotatef(cubeAngle, 1.0, 1.0, 1.0);
 	glTranslatef(-cubeX, cubeY, 0.0);// go to: origin
-	
+
 	cube(cubeX, -cubeY, 0.0, cubeSize);
-	
+
 	glFlush();
 }
 
@@ -335,23 +335,23 @@ void display()
 //================================
 int main(int argc, char *argv[])
 {
-    // OpenGL setup
+	// OpenGL setup
 	glutInit(&argc, argv);
 	glutInitWindowSize(X_SCREEN, Y_SCREEN);
 	glutInitWindowPosition(80, 80);
 	glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE | GLUT_DEPTH);
 	glutCreateWindow("Graphics Project 3--Serna-Aguilera");
-	
+
 	init();
-	
-    // OpenGL functions with callbacks
-    glutMouseFunc(mouse);
-    glutMotionFunc(motion);
-    glutDisplayFunc(display);
-    glutTimerFunc(SLEEP_TIME, timer, 0);
-    //glutIdleFunc(idle);
-	
+
+	// OpenGL functions with callbacks
+	glutMouseFunc(mouse);
+	glutMotionFunc(motion);
+	glutDisplayFunc(display);
+	glutTimerFunc(SLEEP_TIME, timer, 0);
+	//glutIdleFunc(idle);
+
 	glutMainLoop();
 
-    return 0;
+	return 0;
 }
